eba0dd21b0cbf117d28bf819fb7f7f39217ba142
danchek123 <116288482+danchek123@users.noreply.github.com>
2022-10-20 17:25:29 +0300

    Create chet.lua

---
+++ chet.lua
@@ -0,0 +1,32 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("Danchek", "RJTheme3")
+
+local Tab = Window:NewTab("SBR")
+
+local Section = Tab:NewSection("SBR")
+
+Section:NewButton("SBR Spavn", "ButtonInfo", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  CFrame.new(-1390.05286, 312.543213, -737.851624, -0.722435892, -2.06330615e-08, -0.69143796, 7.00832237e-09, 1, -3.71633142e-08, 0.69143796, -3.16939328e-08, -0.722435892)
+end)
+
+Section:NewButton("1 Stag", "ButtonInfo", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  CFrame.new(-1416.17151, 266.112427, 2837.83887, -0.981607854, -9.54351407e-08, -0.190908268, -8.98462531e-08, 1, -3.79310379e-08, 0.190908268, -2.00810124e-08, -0.981607854)
+end)
+
+Section:NewButton("2 Stag", "ButtonInfo", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  CFrame.new(-1173.2196, 275.969971, 3834.27393, -0.998592257, -8.68809025e-09, -0.0530426353, -1.15543317e-08, 1, 5.37299059e-08, 0.0530426353, 5.42671401e-08, -0.998592257)
+end)
+
+Section:NewButton("3 Stag", "ButtonInfo", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  CFrame.new(-892.799866, 410.11322, 6071.93652, -0.829863667, -3.67930042e-08, -0.557966232, -6.51432188e-08, 1, 3.09462891e-08, 0.557966232, 6.20289171e-08, -0.829863667)
+end)
+
+Section:NewButton("4 Stag", "ButtonInfo", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  CFrame.new(-1379.69958, 413.116272, 7970.18799, -0.998430729, 0, 0.0560017154, 0, 1, -0, -0.0560017228, 0, -0.99843061)
+end)
+
+Section:NewButton("Finish", "ButtonInfo", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  CFrame.new(-1836.62146, 415.920898, 8971.22949, -0.222091794, -0.0709823892, 0.972438574, 0.0632502809, 0.994196951, 0.0870161355, -0.972972035, 0.0808325782, -0.216313332)
+end)
+
